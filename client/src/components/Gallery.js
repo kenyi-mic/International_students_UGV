806855afd17313c4e0c96e8a47c098f016d0506e
Michael Kenyi <michaelkenyi13@gmail.com>
2021-12-03 10:52:02 +0600

    updates...

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,62 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Post from "./Post/post";
-import { Grid, CircularProgress } from "@material-ui/core";
+import { CircularProgress } from "@material-ui/core";
 
 function Gallery() {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 
   return !posts.length ? (
-    <CircularProgress />
+    <CircularProgress className="relative top-2/4 right-2/4 " />
   ) : (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-      {posts
-        .slice(0, 4)
-        .map(({ title, description, createdAt, image, creator }) => (
-          <Post
-            key={id}
-            id={id}
-            title={title}
-            description={description}
-            createdAt={createdAt}
-            image={image}
-            creator={creator}
-          />
-        ))}
-      {/* <img
-          className="md:col-span-full"
-          src="https://links.papareact.com/dyz"
-          alt=""
-        /> */}
-      <div className="md:col-span-2">
-        {products
-          .slice(4, 5)
-          .map(({ id, title, price, description, category, image }) => (
-            <Post
-              key={id}
-              id={id}
-              title={title}
-              price={price}
-              description={description}
-              category={category}
-              image={image}
-            />
-          ))}
-      </div>
-      {products
-        .slice(5, posts.length)
-        .map(({ id, title, description, createdAt, image, creator }) => (
-          <Post
-            key={id}
-            id={id}
-            title={title}
-            description={description}
-            createdAt={createdAt}
-            image={image}
-            creator={creator}
-          />
-        ))}
+      {posts.map((post) => (
+        <div key={post._id} className="">
+          <Post post={post} />
+        </div>
+      ))}
     </div>
   );
 }
--- client/src/components/Post/post.js
+++ client/src/components/Post/post.js
@@ -1,11 +1,19 @@
 import React from "react";
+import moment from "moment";
 
-function post() {
+function Post({ post }) {
   return (
-    <div>
-      <h1>Here's my first post!</h1>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-grsy-400">
+        {post.creator}
+      </p>
+      <img src={post.image} width={200} height={200} className="w-100 m-auto" />
+      <h4 className="my-3">{post.title}</h4>
+      <p className="text-xs my-2 line-clamp-2">{post.description}</p>
+      <div className="mb-5">{moment(post.createdAt).fromNow()}</div>
+      <button className="button">Learn More</button>
     </div>
   );
 }
 
-export default post;
+export default Post;
